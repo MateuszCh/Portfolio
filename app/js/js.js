68927063ca77574da2630e11a27c8ade8c764f5e
Mateusz Chybiorz <mateusz.chybiorz@gmail.com>
2016-12-22 02:12:50 +0100

    change of slides by keyboard added, bug with slides fixed

--- app/js/js.js
+++ app/js/js.js
@@ -4,57 +4,77 @@
 $(function() {
     var $headers = $(".headerToSlide");
     var $slides = $(".slides");
-    $($slides[1]).addClass("moveRight");
-    $($slides[2]).addClass("moveLeft");
+    var $body = $("body");
+    var $nextSlider;
+    var color;
+    var $lastSlider;
+    var targetSlider;
+    $($slides[1]).hide().addClass("moveRight");
+    $($slides[2]).hide().addClass("moveLeft");
+    function next() {
+        if($($targetSlider).next()[0]){
+            $nextSlider = $($targetSlider).next()[0];
+            $lastSlider = $($nextSlider).next()[0];
+            if(!$lastSlider){
+                $lastSlider = $(".slides")[0];
+            }
+        } else {
+            $nextSlider = $slides[0];
+            $lastSlider = $slides[1];
+        }
+        $($nextSlider).show(1);
+        $($targetSlider).hide(1);
+        $($lastSlider).hide(1);
+        color = $($nextSlider).data("color");
+        $body.removeClass("light").removeClass("dark").addClass(color);
+        $($targetSlider).addClass("moveLeft");
+        $($nextSlider).removeClass("moveRight");
+        $($lastSlider).removeClass("moveLeft").addClass("moveRight");
+    }
+    function previous() {
+        if($($targetSlider).prev()[0]){
+            $nextSlider = $($targetSlider).prev()[0];
+            $lastSlider = $($nextSlider).prev()[0];
+            if(!$lastSlider){
+                $lastSlider = $(".slides")[2];
+            }
+        } else {
+            $nextSlider = $slides[2];
+            $lastSlider = $slides[1];
+        }
+        $($nextSlider).show(1);
+        $($targetSlider).hide(1);
+        $($lastSlider).hide(1);
+        color = $($nextSlider).data("color");
+        $body.removeClass("light").removeClass("dark").addClass(color);
+        $($targetSlider).addClass("moveRight");
+        $($nextSlider).removeClass("moveLeft");
+        $($lastSlider).removeClass("moveRight").addClass("moveLeft");
+    }
     $headers.swipe( {
         swipe:function(event, direction) {
-            var $targetSlider = $(this).parent().parent()[0];
-            var $body = $("body");
-            var $nextSlider;
-            var color;
-            var $lastSlider;
+            $targetSlider = $(this).parent().parent()[0];
             switch(direction){
                 case "left":
-                    if($($targetSlider).next()[0]){
-                        $nextSlider = $($targetSlider).next()[0];
-                        $lastSlider = $($nextSlider).next()[0];
-                        if(!$lastSlider){
-                            $lastSlider = $(".slides")[0];
-                        }
-                    } else {
-                        $nextSlider = $slides[0];
-                        $lastSlider = $slides[1];
-                    }
-                    $($nextSlider).show(1);
-                    $($targetSlider).hide(1);
-                    $($lastSlider).hide(1);
-                    color = $($nextSlider).data("color");
-                    $body.removeClass("light").removeClass("dark").addClass(color);
-                    $($targetSlider).addClass("moveLeft");
-                    $($nextSlider).removeClass("moveRight");
-                    $($lastSlider).removeClass("moveLeft").addClass("moveRight");
+                    next();
                     break;
                 case "right":
-                    if($($targetSlider).prev()[0]){
-                        $nextSlider = $($targetSlider).prev()[0];
-                        $lastSlider = $($nextSlider).prev()[0];
-                        if(!$lastSlider){
-                            $lastSlider = $(".slides")[2];
-                        }
-                    } else {
-                        $nextSlider = $slides[2];
-                        $lastSlider = $slides[1];
-                    }
-                    $($nextSlider).show(1);
-                    $($targetSlider).hide(1);
-                    $($lastSlider).hide(1);
-                    color = $($nextSlider).data("color");
-                    $body.removeClass("light").removeClass("dark").addClass(color);
-                    $($targetSlider).addClass("moveRight");
-                    $($nextSlider).removeClass("moveLeft");
-                    $($lastSlider).removeClass("moveRight").addClass("moveLeft");
+                    previous();
                     break;
             }
+        }
+    });
+    $(document).on("keydown", function (e) {
+        $($slides).each(function () {
+            if(!($(this).hasClass("moveLeft") || $(this).hasClass("moveRight"))){
+                $targetSlider = $(this);
+            }
+        });
+        var code = e.which;
+        if(code == 37){
+            previous();
+        } else if (code == 39){
+            next();
         }
     });
 });
